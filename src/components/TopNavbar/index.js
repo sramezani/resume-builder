d4ed22550d6f77bfb48be81564425166a557cbc1
Siamak <siamak.rp@gmail.com>
2020-02-24 00:00:51 +0800

    top navbar (background color change)

--- src/components/TopNavbar/index.js
+++ src/components/TopNavbar/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SketchPicker, ChromePicker, BlockPicker, PhotoshopPicker, TwitterPicker, CirclePicker } from 'react-color';
+
 import Util from '../../lib/Util';
+import AppConfig from '../../constants/config';
+
+import { appStore } from '../../redux/store';
+import { updateTheme } from '../../redux/core/actions';
 
 
 import styles from './style.scss';
@@ -11,16 +17,60 @@ class TopNavbar extends React.Component{
         super(props);
 
         this.state = {
+            colorPicker: false,
+            bgComplete: false
         }
 
     }
 
-    render(){
+    handleChangeComplete = (color) => {
+        this._bgPress();
+        this.setState({ background: color.hex });
+        const data = {
+            color: color.hex
+        }
+        appStore.dispatch(updateTheme(data));
+    };
 
+    _colorBtnPress = () => {
+        // this._bgPress();
+        this.setState({
+            bgComplete: true,
+            colorPicker: true
+        })
+    }
+
+    _bgPress = () => {
+        this.setState({
+            colorPicker: false,
+            bgComplete: false
+        });
+    }
+
+    render(){
+        let { colorPicker, bgComplete } = this.state;
         return (
+            <>
+            {
+                bgComplete &&
+                    <div className="bg-complete" onClick={this._bgPress} />
+            }
             <div className="TopNavbar">
-                we
+                <div className="tonNavbar-color-btn" onClick={this._colorBtnPress}>
+                    color
+                </div>
+                {
+                    colorPicker &&
+                        <CirclePicker
+                            color={ this.state.background }
+                            width={134}
+                            className="topNavbar-circle-picker"
+                            colors={AppConfig.materialColors}
+                            onChangeComplete={ this.handleChangeComplete }
+                        />
+                }
             </div>
+            </>
         )
      }
 }
--- src/constants/config.js
+++ src/constants/config.js
@@ -6,6 +6,6 @@
 
 export default {
     
-    
+    materialColors: ['#4CAF50', '#FF5722', '#607D8B', '#C0CA33', '#03A9F4', '#009688', '#673AB7', '#ee0290', '#D32F2F']
 
 };
