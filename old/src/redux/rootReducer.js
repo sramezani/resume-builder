a847ee82cb38f883e3e10220daefc0ddcc5190a2
Siamak <siamak.rp@gmail.com>
2020-03-29 18:07:34 +0800

    delete old (CRA) files

--- old/src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from 'redux';
-import core from './core/reducer'; //put any name
-
-// const rootReducer = combineReducers({
-//     core: core
-// })
-export default core;
