4cf86f72962af017587109c1ea4dec3ed4bc8c75
Siamak <siamak.rp@gmail.com>
2020-06-24 22:24:15 +0800

    404 not found page

---
+++ pages/404/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+
+import { HeaderNavbar, Footer } from '@component';
+
+import styles from './style.module.scss';
+
+export default function Custom404() {
+    return (
+        <div className={styles.errPage}>
+            <Head>
+                <title>404 Not Found  | wtfresume</title>
+            </Head>
+            <HeaderNavbar />
+
+            <div className="container">
+                <div className={styles.insideErr}>
+                    <h1>404 | Page Not Found</h1>
+                    <Link href="/">
+                        <a>
+                            Home Page
+                        </a>
+                    </Link>
+                </div>
+            </div>
+            
+            <Footer />
+        </div>
+    )
+}
