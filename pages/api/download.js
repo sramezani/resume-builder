6dfbbb06e966bce8630121c1353ad91bfc2122af
Siamak <siamak.rp@gmail.com>
2021-04-16 18:33:58 +0800

    remove mongodb

---
+++ pages/api/download.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+const { v1: uuidv1 } = require('uuid');
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const uuid = uuidv1();
+        const pdfId = { pdfId: uuid };
+        const data = { ...pdfId, ...req.body };
+
+        const generatePDF = async () => {
+            // const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            const page = await browser.newPage();
+            await page.goto(`${process.env.HOST}/preview?export=true#${encodeURIComponent(JSON.stringify(data))}`, {
+                waitUntil: 'networkidle2',
+            });
+            const pdf = await page.pdf({
+                printBackground: true,
+                format: 'A4',
+                width: '210mm',
+                height: '297mm',
+            });
+            await browser.close();
+
+            res.send(pdf);
+        };
+        generatePDF();
+    }
+}
--- src/constant/api.js
+++ src/constant/api.js
@@ -5,13 +5,13 @@
 
 export default {
     // The URL we're connecting to
-    // hostname: 'http://localhost:3007',
-    hostname: 'https://api.wtfresume.com',
+    hostname: 'http://localhost:3000',
+    // hostname: 'https://api.wtfresume.com',
     imgurHostname: 'https://api.imgur.com/3/image',
 
     endpoints: [
-        { key: 'save', url: '/save', method: 'POST' },
-        { key: 'download', url: '/download', method: 'GET' },
-        { key: 'download', url: '/download', method: 'POST' },
+        { key: 'save', url: '/api/save', method: 'POST' },
+        { key: 'download', url: '/api/download', method: 'GET' },
+        { key: 'download', url: '/api/download', method: 'POST' },
     ],
 };
